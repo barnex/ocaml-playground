67ec53965cea3f7e6a33bcb0a16175de717c9118
Arne Vansteenkiste <a.vansteenkiste@gmail.com>
2016-06-23 21:41:20 +0100

    hello.ml

--- hello.ml
+++ hello.ml
@@ -3,4 +3,10 @@
 let average a b =
         (a +. b) /. 2.0;;
 
-let () = print_endline (string (average 1. 2.))
+let avg a b =
+        let sum = a +. b in
+        sum /. 2.0;;
+
+let () = 
+        print_endline (string_of_float (average 1. 2.));
+        print_endline (string_of_float (avg 1. 2.));
