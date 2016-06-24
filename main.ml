5889ec194c44cf8fffb1aef2ccb160ca9e7b3ac0
Arne Vansteenkiste <a.vansteenkiste@gmail.com>
2016-06-24 23:45:08 +0100

    cleanup

--- main.ml
+++ main.ml
@@ -9,6 +9,7 @@ let bally = ref (boxh/2)
 let vx = ref 2
 let vy = ref 1
 
+
 let draw (): unit =
     clear_graph ();
 
@@ -16,18 +17,23 @@ let draw (): unit =
     draw_rect q q (boxw - q) (boxh - q);
     fill_circle !ballx !bally q;
 
-    synchronize ();;
+    synchronize ();
+;;
+
 
 let rec sleep secs: unit =
-        try ignore (Unix.select [] [] [] 0.01) with
-        Unix.Unix_error(Unix.EINTR, "select", "") -> sleep secs;;
+    try ignore (Unix.select [] [] [] 0.01) with
+    Unix.Unix_error(Unix.EINTR, "select", "") -> sleep secs;
+;;
+
 
 let rec sleep_until until: unit =
-        let secs = (until -. Unix.gettimeofday()) in
-        if secs > 0.0 then
-            try ignore (Unix.select [] [] [] secs) with
-            Unix.Unix_error(Unix.EINTR, "select", "") -> sleep_until until
-    ;;
+     let secs = (until -. Unix.gettimeofday()) in
+     if secs > 0.0 then
+         try ignore (Unix.select [] [] [] secs) with
+         Unix.Unix_error(Unix.EINTR, "select", "") -> sleep_until until
+;;
+
 
 let main (): unit =
     open_graph "";
@@ -43,6 +49,8 @@ let main (): unit =
         sleep_until(start +. (1.0/.60.0));
     done;
     ignore(wait_next_event [Mouse_motion; Button_down; Button_up; Key_pressed]);
+;; 
 
-;; main ()
+
+main ()
     
