e440f6507c635fd4bc8aee30de5a9c88aa4033a2
Arne Vansteenkiste <a.vansteenkiste@gmail.com>
2016-06-24 21:37:08 +0100

    draw a rectangle

--- main.ml
+++ main.ml
@@ -1,7 +1,16 @@
 open Graphics
 
 let main (): unit =
-        open_graph "";
+    open_graph "";
+    auto_synchronize false; 
+    set_color black;
+
+    let q = 10 in
+    fill_rect q q (2*q) (10*q);
+
+    synchronize ();
+
+    ignore(wait_next_event [Mouse_motion; Button_down; Button_up; Key_pressed]);
 
 ;; main ()
     
