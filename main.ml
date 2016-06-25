d581303b4de26b63fdcb52add9346c74c54e1d10
Arne Vansteenkiste <a.vansteenkiste@gmail.com>
2016-06-25 11:12:09 +0100

    debug output

--- main.ml
+++ main.ml
@@ -3,11 +3,31 @@ open Graphics
 let boxw = 500
 let boxh = 300
 let q = 10
+let texth = 16
 
 let ballx = ref (boxw/2)
 let bally = ref (boxh/2)
 let vx = ref 2
 let vy = ref 1
+
+let debug_point label xy: unit =
+        let x, y = xy in
+        draw_string label;
+        draw_string " ";
+        draw_string (string_of_int x);
+        draw_string " ";
+        draw_string (string_of_int y);
+        moveto (2*q) (current_y() - texth);
+;;
+
+let debug_status (): unit =
+        set_color blue;
+        moveto (2*q) (boxh - 2*q);
+
+        debug_point "mouse_pos" (mouse_pos ());
+        debug_point "ball"      (!ballx, !bally);
+        debug_point "v"         (!vx, !vy);
+;;
 
 
 let draw (): unit =
@@ -16,14 +36,11 @@ let draw (): unit =
     set_color black;
     draw_rect q q (boxw - q) (boxh - q);
     fill_circle !ballx !bally q;
+    moveto (!ballx + q) !bally;
+    draw_string "Felix";
 
-    synchronize ();
-;;
+    debug_status ();
 
-
-let rec sleep secs: unit =
-    try ignore (Unix.select [] [] [] 0.01) with
-    Unix.Unix_error(Unix.EINTR, "select", "") -> sleep secs;
 ;;
 
 
@@ -33,6 +50,7 @@ let rec sleep_until until: unit =
          try ignore (Unix.select [] [] [] secs) with
          Unix.Unix_error(Unix.EINTR, "select", "") -> sleep_until until
 ;;
+
 
 
 let main (): unit =
@@ -47,8 +65,8 @@ let main (): unit =
         vy := if !bally <= 2*q || !bally >= boxh - q then -(!vy) else (!vy);
         draw ();
         sleep_until(start +. (1.0/.60.0));
+        synchronize ();
     done;
-    ignore(wait_next_event [Mouse_motion; Button_down; Button_up; Key_pressed]);
 ;; 
 
 
