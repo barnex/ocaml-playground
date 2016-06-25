bd1ca21b5933d3bc6dc7cda308bb88884134f53a
Arne Vansteenkiste <a.vansteenkiste@gmail.com>
2016-06-25 12:35:10 +0100

    draw pads

--- main.ml
+++ main.ml
@@ -11,9 +11,14 @@ let boxw = 500
 let boxh = 300
 let q = 10
 let texth = 16
+let padw = q
+let padh = 10*q
 
 let v = point 2 1
 let ball = point (boxw/2) (boxh/2)
+
+let pad1 = point (2*q) (boxh/2)
+let pad2 = point (boxw-q) (boxh/2)
 
 
 let debug_point label xy: unit =
@@ -28,12 +33,16 @@ let debug_point label xy: unit =
 
 
 let debug_status (): unit =
-        set_color blue;
+        set_color green;
         moveto (2*q) (boxh - 2*q);
 
         debug_point "mouse_pos" (mouse_pos ());
         debug_point "ball"      (ball.x, ball.y);
         debug_point "v"         (v.x, v.y);
+;;
+
+let draw_pad pad: unit =
+        fill_rect (pad.x-padw/2) (pad.y-padh/2) padw padh
 ;;
 
 
@@ -43,8 +52,12 @@ let draw (): unit =
     set_color black;
     draw_rect q q (boxw - q) (boxh - q);
     fill_circle ball.x ball.y q;
-    moveto (ball.x + q) ball.y;
-    draw_string "Felix";
+
+    set_color red;
+    draw_pad pad1;
+
+    set_color blue;
+    draw_pad pad2;
 
     debug_status ();
 
