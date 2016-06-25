89d77296a20d0f22e2d1f3dee9c430b1c3edae11
Arne Vansteenkiste <a.vansteenkiste@gmail.com>
2016-06-25 12:28:10 +0100

    more records

--- main.ml
+++ main.ml
@@ -13,8 +13,7 @@ let q = 10
 let texth = 16
 
 let v = point 2 1
-let ballx = ref (boxw/2)
-let bally = ref (boxh/2)
+let ball = point (boxw/2) (boxh/2)
 
 
 let debug_point label xy: unit =
@@ -33,7 +32,7 @@ let debug_status (): unit =
         moveto (2*q) (boxh - 2*q);
 
         debug_point "mouse_pos" (mouse_pos ());
-        debug_point "ball"      (!ballx, !bally);
+        debug_point "ball"      (ball.x, ball.y);
         debug_point "v"         (v.x, v.y);
 ;;
 
@@ -43,8 +42,8 @@ let draw (): unit =
 
     set_color black;
     draw_rect q q (boxw - q) (boxh - q);
-    fill_circle !ballx !bally q;
-    moveto (!ballx + q) !bally;
+    fill_circle ball.x ball.y q;
+    moveto (ball.x + q) ball.y;
     draw_string "Felix";
 
     debug_status ();
@@ -67,10 +66,10 @@ let main (): unit =
 
     for i = 1 to 10000000 do 
         let start = Unix.gettimeofday() in
-        ballx := !ballx + v.x;
-        bally := !bally + v.y;
-        v.x <- if !ballx <= 2*q || !ballx >= boxw - q then -v.x else v.x;
-        v.y <- if !bally <= 2*q || !bally >= boxh - q then -v.y else v.y;
+        ball.x <- ball.x + v.x;
+        ball.y <- ball.y + v.y;
+        v.x <- if ball.x <= 2*q || ball.x >= boxw - q then -v.x else v.x;
+        v.y <- if ball.y <= 2*q || ball.y >= boxh - q then -v.y else v.y;
         draw ();
         sleep_until(start +. (1.0/.60.0));
         synchronize ();
