70166247d0db04451d5c264219becc529d7639de
Arne Vansteenkiste <a.vansteenkiste@gmail.com>
2016-06-25 12:26:35 +0100

    records

--- main.ml
+++ main.ml
@@ -1,14 +1,21 @@
 open Graphics
 
+type point = {mutable x: int; mutable y: int}
+
+let point x y: point =
+        {x=x; y=y};
+;;
+
+
 let boxw = 500
 let boxh = 300
 let q = 10
 let texth = 16
 
+let v = point 2 1
 let ballx = ref (boxw/2)
 let bally = ref (boxh/2)
-let vx = ref 2
-let vy = ref 1
+
 
 let debug_point label xy: unit =
         let x, y = xy in
@@ -20,13 +27,14 @@ let debug_point label xy: unit =
         moveto (2*q) (current_y() - texth);
 ;;
 
+
 let debug_status (): unit =
         set_color blue;
         moveto (2*q) (boxh - 2*q);
 
         debug_point "mouse_pos" (mouse_pos ());
         debug_point "ball"      (!ballx, !bally);
-        debug_point "v"         (!vx, !vy);
+        debug_point "v"         (v.x, v.y);
 ;;
 
 
@@ -54,15 +62,15 @@ let rec sleep_until until: unit =
 
 
 let main (): unit =
-    open_graph "";
+    open_graph " 510x310";
     auto_synchronize false; 
 
     for i = 1 to 10000000 do 
         let start = Unix.gettimeofday() in
-        ballx := !ballx + !vx;
-        bally := !bally + !vy;
-        vx := if !ballx <= 2*q || !ballx >= boxw - q then -(!vx) else (!vx);
-        vy := if !bally <= 2*q || !bally >= boxh - q then -(!vy) else (!vy);
+        ballx := !ballx + v.x;
+        bally := !bally + v.y;
+        v.x <- if !ballx <= 2*q || !ballx >= boxw - q then -v.x else v.x;
+        v.y <- if !bally <= 2*q || !bally >= boxh - q then -v.y else v.y;
         draw ();
         sleep_until(start +. (1.0/.60.0));
         synchronize ();
