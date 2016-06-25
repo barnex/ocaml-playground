1d8fa9ed83cf51c013089cbb2d0eae2408cee4d6
Arne Vansteenkiste <a.vansteenkiste@gmail.com>
2016-06-25 21:09:12 +0100

    rect

--- geom.ml
+++ geom.ml
@@ -6,3 +6,8 @@ let point x y: point =
 ;;
 
 
+type rect = {mutable x1: int; mutable y1: int; mutable x2: int; mutable y2: int}
+
+let rect x1 y1 x2 y2: rect =
+        {x1=x1; y1=y1; x2=x2; y2=y2};
+;;
