3f7bc67f7a75cf25f320ffb70e91fdc1a99d09ec
Arne Vansteenkiste <a.vansteenkiste@gmail.com>
2016-06-25 21:17:51 +0100

    start int->float refactoring

--- geom.ml
+++ geom.ml
@@ -1,13 +1,26 @@
+open Graphics
 
-type point = {mutable x: int; mutable y: int}
+type point = {
+        mutable x: float;
+        mutable y: float
+}
 
-let point x y: point =
-        {x=x; y=y};
+let point x y: point = {x=x; y=y};
 ;;
 
 
-type rect = {mutable x1: int; mutable y1: int; mutable x2: int; mutable y2: int}
+type rect = {
+        mutable x1: float;
+        mutable y1: float;
+        mutable x2: float;
+        mutable y2: float;
+}
 
 let rect x1 y1 x2 y2: rect =
         {x1=x1; y1=y1; x2=x2; y2=y2};
+;;
+
+
+let fill_rectf x y w h: unit =
+        fill_rect (int_of_float x) (int_of_float y) (int_of_float w) (int_of_float h);
 ;;
