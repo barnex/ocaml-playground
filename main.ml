979108ddc3be6d724c2791700f5dab1288cff7db
Arne Vansteenkiste <a.vansteenkiste@gmail.com>
2016-06-25 22:35:29 +0100

    refactor padr

--- main.ml
+++ main.ml
@@ -8,17 +8,18 @@ let boxh = 300.0
 let q = 10.0
 let padw = q
 let padh = 10.0 *. q
+let padr = padh /. 2.0
 let pad_speed = 4.0
 
-let v    = point 2.5           1.5
+let v    = point 3.5           2.5
 let ball = point (boxw /. 2.0) (boxh /. 2.0)
 
-let pad1 = point (4.0 *. q)        (boxh /. 2.0)
+let pad1 = point (4.0 *. q)         (boxh /. 2.0)
 let pad2 = point (boxw -. 3.0*.q)   (boxh /. 2.0)
 
 
 let draw_pad pad: unit =
-    fill_rectf (pad.x -. padw /. 2.0) (pad.y -. padh /. 2.0) padw padh
+    fill_rectf (pad.x -. padw /. 2.0) (pad.y -. padr) padw padh
 ;;
 
 
@@ -58,12 +59,12 @@ let move_ball (): unit =
     v.x <- if ball.x <= 2.0*.q || ball.x >= boxw -. q then -.v.x else v.x;
     v.y <- if ball.y <= 2.0*.q || ball.y >= boxh -. q then -.v.y else v.y;
 
-    let top = pad1.y +. (padh /. 2.0) in
-    let bottom = pad1.y -. (padh /. 2.0) in
+    let top = pad1.y +. padr in
+    let bottom = pad1.y -. padr in
     if v.x < 0.0 && ball.x <= pad1.x +. q && ball.y <= top && ball.y >= bottom then v.x <- -.v.x;
 
-    let top = pad2.y +. (padh /. 2.0) in
-    let bottom = pad2.y -. (padh /. 2.0) in
+    let top = pad2.y +. padr in
+    let bottom = pad2.y -. padr in
     if v.x > 0.0 && ball.x >= pad2.x -. q && ball.y <= top && ball.y >= bottom then v.x <- -.v.x;
 ;;
 
@@ -73,7 +74,7 @@ let move_pad1 (): unit =
     let x,y = mouse_posf() in
     if y > pad1.y then pad1.y <- pad1.y +. pad_speed;
     if y < pad1.y then pad1.y <- pad1.y -. pad_speed;
-    pad1.y <- (limit (pad1.y) (padh/.2.0+.q) (boxh-.padh/.2.0));
+    pad1.y <- (limit (pad1.y) (padr+.q) (boxh-.padr));
 ;;
 
 
@@ -83,8 +84,8 @@ let move_pad2 (): unit =
         let y = ball.y in
 
         if v.x > 0.0 then (
-                if y > pad2.y +. padh/.2.0 then aiv := !aiv +. 1.0;
-                if y < pad2.y -. padh/.2.0 then aiv := !aiv -. 1.0;
+                if y > pad2.y +. padr then aiv := !aiv +. 1.0;
+                if y < pad2.y -. padr then aiv := !aiv -. 1.0;
         ) else (
                 aiv := !aiv /. 2.0;
         );
@@ -92,7 +93,7 @@ let move_pad2 (): unit =
         aiv := if !aiv > pad_speed then pad_speed else !aiv;
         aiv := if !aiv < -.pad_speed then -.pad_speed else !aiv;
 
-        pad2.y <- (limit (pad2.y +. !aiv) (padh/.2.0+.q) (boxh-.padh/.2.0));
+        pad2.y <- (limit (pad2.y +. !aiv) (padr+.q) (boxh-.padr));
 ;;
 
 
