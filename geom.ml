97a51210d323ad31ec0a6bc1b5415d3cf7d4aab2
Arne Vansteenkiste <a.vansteenkiste@gmail.com>
2016-06-26 21:18:03 +0100

    cleanup

--- geom.ml
+++ geom.ml
@@ -9,55 +9,6 @@ let point x y: point = {x=x; y=y};
 ;;
 
 
-type rect = {
-        mutable x: float;
-        mutable y: float;
-        mutable rx: float;
-        mutable ry: float;
-}
-
-let rect_of_bounds x1 y1 x2 y2: rect = {
-        x  = (x1+.x2)/.2.0; 
-        y  = (y1+.y2)/.2.0;
-        rx = (x2-.x1)/.2.0; 
-        ry = (y2-.y1)/.2.0;
-    }
-;;
-
-let rect_of_center_w_h x y w h: rect = {
-        x = x;
-        y = y;
-        rx = w /. 2.0;
-        ry = h /. 2.0;
-    }
-;;
-
-
-let x1 (r:rect) = 
-        r.x -. r.rx;
-;;
-
-let x2 (r:rect) = 
-        r.x +. r.rx;
-;;
-
-let y1 (r:rect) = 
-        r.y -. r.ry;
-;;
-
-let y2 (r:rect) = 
-        r.y +. r.ry;
-;;
-
-let w (r:rect) =
-        2.0 *. r.rx;
-;;
-
-let h (r:rect) =
-        2.0 *. r.ry;
-;;
-
-
 let fill_rectf x y w h: unit =
         fill_rect (int_of_float x) (int_of_float y) (int_of_float w) (int_of_float h);
 ;;
@@ -67,21 +18,18 @@ let draw_rectf x y w h: unit =
         draw_rect (int_of_float x) (int_of_float y) (int_of_float w) (int_of_float h);
 ;;
 
+
 let fill_circlef x y r: unit =
         fill_circle (int_of_float x) (int_of_float y) (int_of_float r);
 ;;
 
-let draw_rect2 r: unit =
-     draw_rectf (x1 r) (y1 r) (w r) (h r);
-;;
 
-
-class boxed (cx:float) (cy:float) (rrx:float) (rry:float) =
+class boxed =
         object (self)
-                val mutable x = cx;
-                val mutable y = cy;
-                val mutable rx = rrx;
-                val mutable ry = rry;
+                val mutable x = 0.0;
+                val mutable y = 0.0;
+                val mutable rx = 0.0;
+                val mutable ry = 0.0;
                 method x () = x;
                 method y () = y;
                 method rx () = rx;
@@ -100,13 +48,13 @@ class boxed (cx:float) (cy:float) (rrx:float) (rry:float) =
                     rx <- (x2-.x1)/.2.0; 
                     ry <- (y2-.y1)/.2.0;
                     self;
-               method set_center_w_h cx cy w h=
+               method set_center_w_h cx cy w h =
                     x <- cx;
                     y <- cy;
                     rx <- w /. 2.0;
                     ry <- h /. 2.0;
                     self;
-               method transl dx dy=
+               method transl dx dy =
                     x <- x +. dx;
                     y <- y +. dy;
         end
@@ -114,11 +62,12 @@ class boxed (cx:float) (cy:float) (rrx:float) (rry:float) =
 
 
 let boxed_of_bounds x1 y1 x2 y2=
-        let b = new boxed 0.0 0.0 0.0 0.0 in
+        let b = new boxed in
         b#set_bounds x1 y1 x2 y2;
 ;;
 
+
 let boxed_of_center_w_h x y w h=
-        let b = new boxed 0.0 0.0 0.0 0.0 in
+        let b = new boxed in
         b#set_center_w_h x y w h;
 ;;
